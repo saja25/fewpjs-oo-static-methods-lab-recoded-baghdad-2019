44508fae938073ff141ac0f5d03c01ed83297e4c
saja amer <sajaamer86@gmail.com>
2019-10-16 07:00:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  } 
+  static sanitize(str){
+   return str.replace(/[^a-zA-Z0-9 '-]/g,"");
+  } 
+  static titleize(sentence){
+    let except=['the','a','an','but','of','and','for','at','by','from']
+    let strs=sentence.split(' ')
+    if (str!==)
+    
+  }
 }
